79c488cc49656c8507b4fc663393c15017ae91e4
Mansab <mansabmirsde@gmail.com>
2022-11-01 12:14:54 +0530

    commit third

--- market/src/api/categoriesAPI.js
+++ market/src/api/categoriesAPI.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
@@ -20,27 +19,4 @@ function CategoriesAPI() {
     }
 }
 
-=======
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-function CategoriesAPI() {
-    const [categories, setCategories] = useState([])
-    const [callback, setCallback] = useState(false)
-
-    useEffect(() => {
-        const getCategories = async () => {
-            const res = await axios.get('/api/category')
-            setCategories(res.data)
-        }
-
-        getCategories()
-    }, [callback])
-    return {
-        categories: [categories, setCategories],
-        callback: [callback, setCallback]
-    }
-}
-
->>>>>>> f8728ffce37ba7587379769914a5dfc40c1c629b
 export default CategoriesAPI
